fadc3ce9949e59b8f06fb9dae99bd625e0ebdd7b
ngoquoctruong <53925768+ngoquoctruong@users.noreply.github.com>
2021-10-22 07:37:00 +0700

    init

---
+++ src/settings.3cfad.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","UI"],collisionMatrix:[[true],[false,true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/landing.fire",orientation:"portrait",jsList:["assets/scripts/server/gta-amplitude.f658d.js"],bundleVers:{internal:"51cdd",resources:"c2b72",main:"ea6d0"}};
